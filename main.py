2292756864a212930f004762e99c8e4c29565184
Alvin Zhu <alvinzhu33@yahoo.com>
2017-02-19 12:55:55 -0500

    Matrix stuff done

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
 
+print_matrix(matrix)
 
-draw_lines( matrix, screen, color )
-display(screen)
+ident(matrix)
+print_matrix(matrix)
+
+scalar_mult(matrix,4)
+print_matrix(matrix)
+
+matrix2 = new_matrix()
+ident(matrix2)
+scalar_mult(matrix2,5)
+print_matrix(matrix2)
+
+matrix_mult(matrix,matrix2)
+print_matrix(matrix2)
+
+#draw_lines( matrix, screen, color )
+#display(screen)
--- matrix.py
+++ matrix.py
@@ -2,18 +2,55 @@ import math
 
 
 def print_matrix( matrix ):
-    pass
+    printing = "";
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            printing+=(str)(matrix[y][x]) + "\t"
+        printing+="\n"
+    print(printing)
 
 def ident( matrix ):
-    pass
+    for y in range(len(matrix)):
+        for x in range (len(matrix[y])):
+            if x == y:
+                matrix[y][x]=1
+            else:
+                matrix[y][x]=0
 
 def scalar_mult( matrix, s ):
-    pass
+    for y in range(len(matrix)):
+        for x in range (len(matrix[y])):
+            matrix[y][x]=matrix[y][x]*s
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
-
+    if(len(m1[0])==len(m2)):
+        m3 = new_matrix()
+        r1=0
+        c=0
+        '''for r in range(len(m1)):
+            s = r
+            for c in range(len(m1[r])):
+                newval = m1[f][s] + m2[s][f]
+            m3[s][f]
+        while r1 < len(m1):
+            r2=0
+            newval = 0
+            while(r2 < len(m1[r1])):
+                newval += m1[r1][r2] * m2[r2][r1]
+                r2+=1
+            m3[r1][c]= newval
+            r1+=1
+            c+=1'''
+        for r1 in range(len(m1)):
+            newval = 0
+            for r2 in range(len(m1[r1])):
+                newval += m1[r1][r2] * m2[r2][r1]
+            m3[r1][c] = newval
+            c+=1
+        for y in range(len(m2)):
+            for x in range(len(m2[y])):
+                m2[y][x] = m3[y][x]
 
 
 
